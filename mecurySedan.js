4b5f0e893e6004a413d215efee370ab1bcf3cee7
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-07-03 16:12:16 -0500

    updating mercurySedan.js

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,12 +1,51 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicle.js");
+const VehicleModule = require("./vehicle").Vehicle;
 
-//this shows how to call from this module...
-let vehicleMercury = new VehicleModule.Vehicle(
-  "Mecury",
-  "Sedan",
-  "1965",
-  "color",
-  "mileage"
-);
-console.log(vehicleMercury.make);
+class Car extends VehicleModule {
+  constructor(make, model, year, color, mileage) {
+    super(make, model, year, color, mileage);
+    this.maxPassengers = 5;
+    this.passenger = 0;
+    this.numberOfWheels = 4;
+    this.maxSpeed = 160;
+    this.fuel = 10;
+    this.scheduleService = false;
+  }
+
+  // if passenger less than maximumPassengers then availableRoom == true
+  loadPassenger(num) {
+    if (this.passenger < this.maxPassengers) {
+      this.availableRoom = true;
+    }
+  }
+
+  // if fuel is greater than 0, then start == true
+  start() {
+    if (this.fuel > 0) {
+      console.log("Car is on");
+      return (this.start = true);
+    } else {
+      console.log("Car needs fuel");
+      return (this.start = false);
+    }
+  }
+
+  // if mileage is greater than 30000, time for maintenance == true
+  checkService(mileage) {
+    if (this.mileage > 30000) {
+      this.scheduleService = true;
+      return this.scheduleService;
+    }
+  }
+}
+
+// console.log({ Car });
+
+let myCar = new Car("Lexus", "IS 350", "2022", "black", 500);
+console.log(myCar);
+
+myCar.start();
+console.log(myCar);
+
+myCar.checkService();
+console.log(myCar);
